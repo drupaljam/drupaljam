badba952429366fe12519749a99542732659d39f
Rolf van de Krol <info@rolfvandekrol.nl>
2011-06-26 13:22:56 +0200

    Setup routine

--- dev/setup.sh
+++ dev/setup.sh
@@ -5,3 +5,17 @@ cd ..
 
 SITEDIR=$(pwd)
 cat src/drupaljam/build.make | sed -e "s|\(projects\[drupaljam\]\[download\]\[url\]\).*|\1 = file://$SITEDIR/src/drupaljam|" > build.make
+
+mkdir data
+chmod a+w data
+
+mkdir config
+
+drush make build.make build
+cp build/sites/default/default.settings.php config/settings.php
+cd build/sites/default
+ln -s ../../../config/settings.php .
+ln -s ../../../data files
+
+cd ../../..
+ln -s src/drupaljam/dev/build.sh .
